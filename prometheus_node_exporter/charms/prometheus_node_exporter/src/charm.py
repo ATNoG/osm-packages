44bf3144dea889446ea047bfc6a2365c0bc319b9
Ubuntu <rdireito@av.it.pt>
2022-01-10 16:52:59 +0000

    prometheus exporter already being downloaded and moved to the correct folder

--- prometheus_node_exporter/charms/prometheus_node_exporter/src/charm.py
+++ prometheus_node_exporter/charms/prometheus_node_exporter/src/charm.py
@@ -5,7 +5,13 @@ sys.path.append("lib")
 
 from charms.osm.sshproxy import SSHProxyCharm
 from ops.main import main
-
+from ops.model import (
+    ActiveStatus,
+    MaintenanceStatus,
+    BlockedStatus,
+    WaitingStatus,
+    ModelError,
+)
 
 class SampleProxyCharm(SSHProxyCharm):
     def __init__(self, framework, key):
@@ -20,6 +26,10 @@ class SampleProxyCharm(SSHProxyCharm):
         # Listen to the touch action event
         self.framework.observe(self.on.configure_remote_action, self.configure_remote)
         self.framework.observe(self.on.start_service_action, self.start_service)
+
+        # Custom actions 
+        self.framework.observe(self.on.start_prometheus_exporter_action, self.on_start_prometheus_exporter)
+
 
     def on_config_changed(self, event):
         """Handle changes in configuration"""
@@ -68,6 +78,77 @@ class SampleProxyCharm(SSHProxyCharm):
         else:
             event.fail("Unit is not leader")
 
+
+    ##########################
+    #    Custom Functions    #
+    ##########################
+
+    def on_start_prometheus_exporter(self, event):
+        self._get_prometheus_exporter(event)
+
+
+    def _get_prometheus_exporter(self, event):
+        may_proceed = True
+
+        self.unit.status = MaintenanceStatus("Getting Prometheus Exporter Bin...")
+        try:
+            cmd = "wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz"
+            proxy = self.get_ssh_proxy()
+            result, error = proxy.run(cmd)
+            event.set_results({"output": result})
+            self.unit.status = MaintenanceStatus("Obtained Prometheus Exporter Tar")
+        except Exception as e:
+            may_proceed = False
+            event.fail("Action failed {}. Stderr: {}".format(e, error))
+            self.unit.status = BlockedStatus("Couldn't Obtain Prometheus Exporter Tar")
+
+        if may_proceed:
+            self.unit.status = MaintenanceStatus("Decompressing Prometheus Exporter Tar...")
+            try:
+                cmd = "tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz"
+                proxy = self.get_ssh_proxy()
+                result, error = proxy.run(cmd)
+                event.set_results({"output": result})
+                self.unit.status = MaintenanceStatus("Decompressed Prometheus Exporter Tar")
+            except Exception as e:
+                may_proceed = False
+                event.fail("Action failed {}. Stderr: {}".format(e, error))
+                self.unit.status = BlockedStatus("Couldn't Decompress Prometheus Exporter Tar")
+        
+        if may_proceed:
+            self.unit.status = MaintenanceStatus("Moving Prometheus Exporter Bin to the Right Location...")
+            try:
+                cmd = "sudo cp node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin/"
+                proxy = self.get_ssh_proxy()
+                result, error = proxy.run(cmd)
+                event.set_results({"output": result})
+                self.unit.status = MaintenanceStatus("Moved Prometheus Exporter Bin to /usr/local/bin/")
+            except Exception as e:
+                may_proceed = False
+                event.fail("Action failed {}. Stderr: {}".format(e, error))
+                self.unit.status = BlockedStatus("Couldn't Move the Prometheus Exporter Bin to /usr/local/bin/")
+
+        if not may_proceed:
+            event.fail("Unable to Get the Prometheus Exporter Binary")
+            self.unit.status = BlockedStatus("Unable to Get the Prometheus Exporter Binary")
+        else:
+            self.unit.status = ActiveStatus("Prometheus Exporter Binary Obatined With Success")
+
+
+    def _create_prometheus_exporter_service(self, event):
+        pass
+
+    def _run_prometheus_exporter(self, event):
+        pass
+
+    def _is_prometheus_exporter_on_machine(self, event):
+        pass
+
+    def _is_prometheus_exporter_service_configured(self, event):
+        pass
+    
+    def _is_prometheus_exporter_service_runnning(self, event):
+        pass
 
 if __name__ == "__main__":
     main(SampleProxyCharm)
